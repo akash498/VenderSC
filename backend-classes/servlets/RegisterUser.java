4bd1069cd29b7fffc6e3abc92806c92d50b29340
Joshua Choe <choejd@usc.edu>
2019-11-26 00:29:43 -0800

    register and login backend stuff

--- backend-classes/servlets/RegisterUser.java
+++ backend-classes/servlets/RegisterUser.java
@@ -17,16 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class LoginValidate
+ * Servlet implementation class RegisterUser
  */
-@WebServlet("/LoginValidate")
-public class LoginValidate extends HttpServlet {
+@WebServlet("/RegisterUser")
+public class RegisterUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginValidate() {
+    public RegisterUser() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,7 +36,6 @@ public class LoginValidate extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
 	}
 
 	/**
@@ -44,72 +43,84 @@ public class LoginValidate extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
 		HttpSession session = request.getSession();
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		String next = "/Login.jsp";
-		String error = "";
+		String confirm = request.getParameter("confirm");
+		
+		String imageLocation = request.getParameter("logo");
+		if (imageLocation == null) imageLocation = "";
+		
+		String next = "/RegisterUser.jsp";
 		Connection conn = null;
 		Statement st = null;
-		PreparedStatement ps = null;
 		ResultSet rs = null;
+		PreparedStatement ps = null;
+		boolean cont = true;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://google/vendorDB?cloudSqlInstance=vendorsc:us-central1:vendor&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=vendor&password=0203");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM Business WHERE username = '"+username+"'");
 			
-			ps = conn.prepareStatement("SELECT * FROM Users WHERE username = ?");
-			ps.setString(1,username);
-			rs = ps.executeQuery();
-			//if username doesnt exist in user table, then check business table
-			if (!rs.next()){    
-				ps = conn.prepareStatement("SELECT * FROM Business WHERE username = ?");
-				ps.setString(1,username);
-				rs = ps.executeQuery();
-				//this username does not exist in either table
-				if (!rs.next()){ 
-					request.setAttribute( "error", "This user does not exist");
-					next = "/Login.jsp";
-				}
-				//this username exists in the business table
-				else {
-					String storedPassword = (String) rs.getObject("password");
-					//wrong password for business username
-					if (!password.contentEquals(storedPassword)) {
-						request.setAttribute( "error", "Incorrect password");
-						next = "/Login.jsp";
-					}
-					//correct password for business username; login business user
-					else {
-						next = "/BusinessHomePage.jsp";
-						session.setAttribute("loggedIn", "true");
-						session.setAttribute("username", username);
-						session.setAttribute("favorite", "true");
-						request.setAttribute("error", "");
-					}
-				}
+			if (username == null || password == null|| confirm == null || username == "" || password == ""|| confirm == "" ) {
+				request.setAttribute( "error", "All fields must be filled");
+				cont = false;
+
 			}
-			// username exists in the user table, so check password
-			else {
-				String storedPassword = (String) rs.getObject("password");
-				if (!password.contentEquals(storedPassword)) {
-					request.setAttribute( "error", "Incorrect password");
-					next = "/Login.jsp";
+			
+			
+			else if (!password.contentEquals(confirm)) {
+				request.setAttribute( "error", "The passwords do not match");
+				cont = false;
+
+			}
+			else if (rs.next()) {    
+				request.setAttribute( "error", "This username is already taken");
+				cont = false;
+
+			}
+			
+			
+			if (cont) {
+			
+			
+			
+				rs = st.executeQuery("SELECT * FROM Users WHERE username = '"+username+"'");
+			
+				if (username == null || password == null|| confirm == null || username == "" || password == ""|| confirm == "" ) {
+					request.setAttribute( "error", "All fields must be filled");
+				}
+			
+			
+				else if (!password.contentEquals(confirm)) {
+					request.setAttribute( "error", "The passwords do not match");
+				}
+				else if (rs.next()) {    
+					request.setAttribute( "error", "This username is already taken");
 				}
 				else {
+				
+					ps = conn.prepareStatement("INSERT INTO Users (username, email, password, name, imageLocation ) VALUES (?,?,?,?,?)");
+					ps.setString(1, username);
+					ps.setString(2, email);
+					ps.setString(3, password);
+					ps.setString(4, name);
+					ps.setString(5, imageLocation);
+					ps.execute();
 					next = "/UserHomePage.jsp";
 					session.setAttribute("loggedIn", "true");
 					session.setAttribute("username", username);
-					session.setAttribute("favorite", "true");
-					request.setAttribute("error", "true");
 				}
-			}	
+			}
+			
 		}
 		catch (SQLException sqle) {  
 			System.out.println(sqle.getMessage());
 			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(next);
 		try {
@@ -120,9 +131,16 @@ public class LoginValidate extends HttpServlet {
 		}
 		catch(ServletException e) {
 			e.printStackTrace();
+		
 		}
-	
-	
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
