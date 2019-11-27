cada864051e1e30e5ac70595da65ae6e0ffede06
Akash Patel <akashppa@usc.edu>
2019-11-27 11:41:59 -0500

    business class finished, need to finish product set reviews and ratings

--- backend-classes/javaFiles/Business.java
+++ backend-classes/javaFiles/Business.java
@@ -1,15 +1,32 @@
 package backend_classes;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 public class Business {
 	private int businessId;
 	private String name;
-	private ArrayList<Integer> products;
-	// constructor
-	Business(int id, String name_) {
-		setBusinessId(id);
-		setName(name_);
-		setProducts(new ArrayList<Integer>());
+	private String description;
+	private ArrayList<Product> products;
+	// constructor - takes in business id and fills in other members from database
+	Business(int id) {
+		businessId = id;
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("url");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM vendorDB.Business WHERE businessID = "+ businessId);
+			name = rs.getString("name");
+			description = rs.getString(("longDescription"));
+		} catch (SQLException e) {
+			System.out.println("erorrr");
+		}
+		setProducts();
 	}
 	// getters and setters
 	public void addProduct(Product p) {
@@ -18,8 +35,8 @@ public class Business {
 	public int getBusinessId() {
 		return businessId;
 	}
-	public void setBusinessId(int businessId) {
-		this.businessId = businessId;
+	public void setBusinessId(int id) {
+		businessId = id;
 	}
 	public String getName() {
 		return name;
@@ -27,12 +44,54 @@ public class Business {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public ArrayList<Integer> getProducts() {
+	public ArrayList<Product> getProducts() {
 		return products;
 	}
-	public void setProducts(ArrayList<Integer> products) {
-		this.products = products;
+	// retrieve products from product table in database for the business id
+	public void setProducts() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		ArrayList<Product> prods = new ArrayList<Product>();
+		try {
+			conn = DriverManager.getConnection("url");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM vendorDB.Product WHERE businessID = "+ businessId);
+			while (rs.next()) {
+				rs.getInt("productID");
+				prods.add(new Product(rs.getString("name"), rs.getString("shortDescription"),rs.getString("longDescription"), rs.getInt("businessID"), rs.getInt("productID")));
+			}
+		} catch (SQLException e) {
+			System.out.println("erorrr");
+		}
+		products = prods;
 	}
-	
+	// adds a product to the product table in the database with the business id
+	public void addProduct(String name, String description, String imagePath, int ratable) {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("url here");
+			st = conn.createStatement();
+			st.executeUpdate("INSERT INTO vendorDB.Product (businessID, name, shortDescription, longDescription, imageLocation, ratable) VALUES ("+ businessId + ", '" + name +"', 'do we want a short descrip', '" + description + "', '" + imagePath + "', " + Integer.toString(ratable) + ")");
+		} catch (SQLException e) {
+			System.out.println("erorrr");
+		}
+		setProducts();
+	}
+	// removes a product from the product table in the database with the business id
+	public void removeProduct(int productId) {
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = DriverManager.getConnection("url here");
+			st = conn.createStatement();
+			st.executeUpdate("DELETE FROM vendorDB.Product WHERE productID = "+ Integer.toString(productId) + ")");
+		} catch (SQLException e) {
+			System.out.println("erorrr");
+		}
+		setProducts();
+	}
 	
 }
--- backend-classes/javaFiles/Product.java
+++ backend-classes/javaFiles/Product.java
@@ -1,21 +1,54 @@
 package backend_classes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Product {
 	private String name;
-	private String price;
+	private String shortDescription;
 	private String description;
 	private int businessId;
 	private double rating;
 	private int productId;
+	private Boolean ratable;
 	private ArrayList<Review> reviews;
-	Product(String name_, String price_, String description_, int businessId_, int productId_) {
+	// constructor
+	Product(String name_, String shortDescription_, String description_, int businessId_, int productId_) {
 		setName(name_);
-		setPrice(price_);
+		setShortDescription(shortDescription_);
 		setDescription(description_);
 		setBusinessId(businessId_);
 		setProductId(productId_);
+		// set the reviews and rating using the rating table in database
 		
-		
+	}
+	// constructor - takes in productId and fills in the rest of members from product table
+	Product(int productId_) {
+		productId = productId_;
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DriverManager.getConnection("url");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM vendorDB.Product WHERE productID = "+ productId);
+			name = rs.getString("name");
+			description = rs.getString(("longDescription"));
+			shortDescription = rs.getString("shortDescription");
+			if (rs.getInt("ratable") == 0 ) {
+				ratable = false;
+			} else {
+				ratable = true;
+			}
+			businessId = rs.getInt("businessID");
+		} catch (SQLException e) {
+			System.out.println("erorrr");
+		}
+		setReviews();
 	}
 	// getters and setters
 	public String getName() {
@@ -23,12 +56,6 @@ public class Product {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getPrice() {
-		return price;
-	}
-	public void setPrice(String price) {
-		this.price = price;
 	}
 	public String getDescription() {
 		return description;
@@ -54,7 +81,19 @@ public class Product {
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
-	public void addReview() {
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	// sets the reviews for this product by calling the rating database and finds the average rating 
+	public void setReviews() {
+		
+	}
+	
+	// add a review for this product, must take in a user's id
+	public void addReview(int userId) {
 		
 	}
 	
