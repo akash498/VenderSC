6b86ddb7001eabcedb5fda72436bfbfb5b124e9b
Akash Patel <akashppa@usc.edu>
2019-11-27 12:06:00 -0500

    added getter and setter for description

--- backend-classes/javaFiles/Business.java
+++ backend-classes/javaFiles/Business.java
@@ -41,6 +41,12 @@ public class Business {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public ArrayList<Product> getProducts() {
 		return products;
 	}
