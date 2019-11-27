fe9eec623755613eb3cae0fc8765c4c73d526db1
tynangangwani <tgangwan@usc.edu>
2019-11-27 15:26:59 -0500

    Review.java

--- backend-classes/javaFiles/Review.java
+++ backend-classes/javaFiles/Review.java
@@ -1,5 +1,35 @@
 package backend_classes;
 
 public class Review {
+	String reviewText;
+	int USerID;
+	int ProductID;
+	double rating;
+
+	public String getReviewText() {
+		return reviewText;
+	}
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+	public int getUSerID() {
+		return USerID;
+	}
+	public void setUSerID(int uSerID) {
+		USerID = uSerID;
+	}
+	public int getProductID() {
+		return ProductID;
+	}
+	public void setProductID(int productID) {
+		ProductID = productID;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
 
 }
